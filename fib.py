b40ea23ad71cd6cb780d187f007e5fab3a859090
Jaden Rodriguez <61155722+YaBoiSkinnyP@users.noreply.github.com>
2023-09-06 01:40:25 -0400

    improved fib/history context

--- fib.py
+++ fib.py
@@ -1,41 +1,42 @@
 # solution to Leetcode 509. Fibonacci number https://leetcode.com/problems/fibonacci-number/description/
+def val(history):
+    match history:
+        case b, _:
+            return b
+        case (val,):
+            return val
+
+def prev(history):
+    match history:
+        case _, h:
+            return h
+        case _:
+            return history
+
+def make_nested_tuple(start, stop):
+    if start == stop:
+        return (start, ())
+    return (start, make_nested_tuple(start + 1, stop))
+
+# nested_tuple-specific catamorphism (fold)
+def cata(f, start_val, nested_tuple):
+    match nested_tuple:
+        case (): # empty
+            return start_val
+        case num, remaining:
+            return f(num, cata(f, start_val, remaining))
+
 class Solution:
-    def cata(self, f, start_val, nested_tuple):
-        match nested_tuple:
-            case num, remaining:
-                return f(num, self.cata(f, start_val, remaining))
-            case _: # empty
-                return start_val
                 
-    def prev(self, history):
-        match history:
-            case _, _, h:
-                return h
-            case _:
-                return history
-
-    def val(self, history):
-        match history:
-            case _, b, _:
-                return b
-            case (val,):
-                return val
-
-    def make_nested_tuple(self, start, stop):
-        if start == stop:
-            return (start, ())
-        return (start, self.make_nested_tuple(start + 1, stop))
-        
+    def f(self, num, history):
+        return val(history) + val(prev(history))
 
     def fib(self, n: int) -> int:
-        if n < 3:
-            return n - (n == 2)
+        if n < 2:
+            return n
 
-        def f(num, history):
-            return self.val(history) + self.val(self.prev(history))
-
-        return self.val(self.cata(
-            lambda num, history: (num, f(num, history), history), 
-            (1,), 
-            self.make_nested_tuple(3, n))
+        return val(cata(
+            lambda num, history: (self.f(num, history), history), 
+            (1, (0,)), 
+            make_nested_tuple(0, n - 2))
             )
